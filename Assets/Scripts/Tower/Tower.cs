9d6b445abab34b2bdb93484c673e0f736a5ddac1
Wing_WR <wingwr998@gmail.com>
2025-01-17 22:58:54 +0800

    20250117:Update Tower.cs

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -4,8 +4,40 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    public TowerAttributes towerAttributes;
+    #region Attributes
+    [Header("Tower Attributes")]
+    [SerializeField] private float health;
+    [SerializeField] private float healthUpper;
+    [SerializeField] private float damage;
+    [SerializeField] private float range;
+    #endregion
 
+    public Tower(float healthUpper, float damage, float range)
+    {
+        this.health = healthUpper;
+        this.healthUpper = healthUpper;
+        this.damage = damage;
+        this.range = range;
+    }
 
+    public void GetHurt(float damage)
+    {
+        this.health -= damage;
+    }
+
+    public bool IsDead()
+    {
+        return health <= 0;
+    }
+
+    public float AttackDamage(Vector2 targetPosition)
+    {
+        if (Vector2.Distance(transform.position, targetPosition) <= range) 
+        {
+            return damage;
+        }
+
+        return 0;
+    }
 
 }
